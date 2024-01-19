750fddeab40cfe64699c81459ce7b0cd3bf36f88
Hmddev23 <hamid.maira709@gmail.com>
2024-01-19 13:18:02 -0800

    exec

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
-#!/usr/bin/pup
-# Install flask from pip3
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# install flask from pip3
+exec {'pip3 install flask':
+  require => Exec['python-installed'],
+  command => '/usr/bin/pip3 install flask==2.1.0'
+}
+
+exec {'python-installed':
+  command => '/usr/bin/which python3'
 }
