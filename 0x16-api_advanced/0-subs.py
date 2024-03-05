e5580e163bfdba24ef3b441a46347dc59eec95dc
Hmddev23 <hamid.maira709@gmail.com>
2024-03-05 05:57:18 -0800

    3 tasks-2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,18 +3,21 @@
 0. How many subs?
 """
 import requests
-
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
     """
     return the number of subscribers for a given subreddit
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,19 +3,22 @@
 1. Top Ten
 """
 import requests
-
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+from sys import argv
 
 
 def top_ten(subreddit):
     """
     print the titles of the first 10 hot posts listed for a given subreddit.
     """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
-    else:
-        print("None")
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
