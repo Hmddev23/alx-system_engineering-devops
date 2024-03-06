c10aab54ffcf7a86104f319893394b126365c567
Hmddev23 <hamid.maira709@gmail.com>
2024-03-06 14:18:40 -0800

    fix 12

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,21 +3,16 @@
 0. How many subs?
 """
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
     """
     return the number of subscribers for a given subreddit
     """
-    user = {'User-Agent': 'MyAlxSchoolProjet v0.0.1'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Alx School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
