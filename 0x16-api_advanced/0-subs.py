eb963275f7bc4f977603c406fbd6ff85189f8e20
Hmddev23 <hamid.maira709@gmail.com>
2024-03-07 03:10:38 -0800

    fix 15

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     return the number of subscribers for a given subreddit
     """
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/1.0(Alx School 0x16 task 0)'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
     if (not response.ok):
         return 0
